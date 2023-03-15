54f24b3555278d2ad41a02052de187fbabe72474
smathot <s.mathot@cogsci.nl>
2023-03-15 20:48:50 +0100

    Add packages information for updater

--- opensesame_extensions/example/__init__.py
+++ opensesame_extensions/example/__init__.py
@@ -2,12 +2,6 @@
 
 __version__ = '0.0.1'
 
-# This information is used by the update checker to see if updates are
-# available. Both pip and packages are supported
-update_sources = [
-    {'type': 'conda',
-     'pkg': 'opensesame-extension-example',
-     'channel': 'conda-forge'},
-    {'type': 'pip',
-     'pkg': 'opensesame-extension-example',
-     'server': 'https://pypi.org'}]
+# These packages will be checked by the updater extension for updates on
+# conda and pip
+packages = ['opensesame-extension-example']
